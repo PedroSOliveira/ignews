7d698f87ef34e69198932f49ed27a9cd8641e5ad
PedroSOliveira <pedrovinicius35@hotmail.com>
2022-05-09 12:00:31 -0300

    feat: Adding authentication with github and integrating with fauna db.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,32 @@
-// import { FaGithub } from "react-icons/fa";
-// import { FiX } from "react-icons/fi";
-// import { signIn, signOut,useSession } from "next-auth/client";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export const SignInButton = () => {
-  return (
-    <button type="button" className={styles.signInButton}>
-      {/* <FaGithub color="#04d361" />
+  const { data: session } = useSession();
+
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
       {session.user.name}
-      <FiX color="#737380" className={styles.closeIcon} /> */}
-      Entrar
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      onClick={() => signIn("github")}
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#eba417" />
+      Sing in with GitHub
     </button>
   );
-  // ) : (
-  //   <button
-  //     onClick={() => signIn("github")}
-  //     type="button"
-  //     className={styles.signInButton}
-  //   >
-  //     <FaGithub color="#eba417" />
-  //     Sing in with GitHub
-  //   </button>
-  // );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,63 @@
+import { GetStaticProps } from "next";
 import Head from "next/Head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
+      <main className={styles.homeContainer}>
+        <section className={styles.homeContent}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to tall the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <div className={styles.hero}>
+          <img
+            className={styles.avatarImg}
+            src="/images/avatar.svg"
+            alt="Avatar"
+          />
+        </div>
+      </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1Kx9JOGZCwQvKGhUjPYzQVUw", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
